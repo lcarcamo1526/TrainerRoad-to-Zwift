e6bba1a18135afd9a85dd0763c74ea19613434a3
Luis Carcamo <lcarcamo1526@universidadean.edu.co>
2021-10-01 12:13:23 -0500

    [WIP] Addex export as zip file

--- trainerroad/Controller/Zwift.py
+++ trainerroad/Controller/Zwift.py
@@ -1,11 +1,21 @@
 import datetime as dt
 import logging
 import os
+import zipfile
 from collections.abc import Iterable
 
 from ..Model.TrainerRoad import TrainerRoad
 from ..Model.Workout import Workout
 from ..Utils.Str import *
+
+
+def gen_zip_from_path(dir_to_archive, archive_filename):
+    ziph = zipfile.ZipFile(archive_filename, 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(dir_to_archive):
+        for file in files:
+            if file != archive_filename:
+                ziph.write(os.path.join(root, file))
+    ziph.close()
 
 
 def create_plan_dictionary(response: Iterable) -> dict:
@@ -42,7 +52,7 @@ class Zwift:
             pass
 
     async def export_training_plan(self, include_date: bool, start_date: str = "12-25-2020",
-                                   end_date: str = "09-25-2023"):
+                                   end_date: str = "09-25-2023", compress=True):
         calendar = self.trainer.get_training_plans(start_date=start_date, end_date=end_date)
         workouts = list(set(calendar[ACTIVITY_ID]))
         response = await self.trainer.get_workouts_details(workouts=workouts)
@@ -75,3 +85,10 @@ class Zwift:
                         logging.error(workout_name)
                 else:
                     logging.error(workout_name)
+
+        if compress:
+            filename = os.path.join(self.output_path, "training_plan.zip")
+            try:
+                gen_zip_from_path(self.output_path, filename)
+            except Exception as e:
+                logging.error(f"Failed to save Zip File: {filename} + {e}")
