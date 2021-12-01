2108dfc7124873cee2727e73febd4df77cf2850a
Luis Carcamo <lcarcamo1526@universidadean.edu.co>
2021-11-30 23:20:08 -0500

    [WIP] Added download link into jupyter  notebook

--- trainerroad/Utils/Str.py
+++ trainerroad/Utils/Str.py
@@ -25,3 +25,5 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3) AppleWebKit/605.1.15 
              "Version/14.1 Safari/605.1.15 "
 
 OUTPUT_FILE = f"training_plan_{today.strftime('%d_%m_%Y_%H_%M_%S')}.zip"
+TRAINERROAD_USER = 'TRAINERROAD_USER'
+TRAINERROAD_PASSWORD = 'TRAINERROAD_PASSWORD'
